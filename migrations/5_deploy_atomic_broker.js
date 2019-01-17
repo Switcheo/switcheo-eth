da4433bcc349fe0fbf78fa7c78754531cf4608d5
John <jwwwwong@gmail.com>
2019-01-17 14:06:03 +0800

    Approve broker in atomicBroker on deploy

--- migrations/5_deploy_atomic_broker.js
+++ migrations/5_deploy_atomic_broker.js
@@ -1,9 +1,13 @@
 const Broker = artifacts.require('Broker')
 const AtomicBroker = artifacts.require('AtomicBroker')
 
-module.exports = function(deployer) {
+module.exports = function(deployer, network, accounts) {
     deployer.then(async () => {
         const broker = await Broker.deployed()
-        return deployer.deploy(AtomicBroker, broker.address)
+        await deployer.deploy(AtomicBroker, broker.address)
+        const atomicBroker = await AtomicBroker.deployed()
+        const owner = accounts[0]
+        await broker.addSpender(atomicBroker.address, { from: owner })
+        await atomicBroker.approveBroker()
     })
 };
