ed876b926feb9c2559d4aa22fdea409858875f21
John <jwwwwong@gmail.com>
2019-02-20 17:25:11 +0800

    Fix AtomicBroker migration

--- migrations/5_deploy_atomic_broker.js
+++ migrations/5_deploy_atomic_broker.js
@@ -1,13 +1,18 @@
 const Broker = artifacts.require('Broker')
 const AtomicBroker = artifacts.require('AtomicBroker')
+const truffleConfig = require('../truffle')
 
 module.exports = function(deployer, network, accounts) {
     deployer.then(async () => {
-        const broker = await Broker.deployed()
+        const { brokerAddress } = process.env
+        const broker = brokerAddress ? await Broker.at(brokerAddress) : await Broker.deployed()
+
         await deployer.deploy(AtomicBroker, broker.address)
         const atomicBroker = await AtomicBroker.deployed()
+
         const owner = accounts[0]
+
         await broker.addSpender(atomicBroker.address, { from: owner })
-        await atomicBroker.approveBroker()
+        await atomicBroker.approveBroker({ from: owner })
     })
 };
