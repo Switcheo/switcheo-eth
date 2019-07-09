757f503a94c04233f31c03d1c777a3abf63b7e9f
John <jwwwwong@gmail.com>
2019-07-09 15:55:40 +0800

    Update solc version

--- truffle.js
+++ truffle.js
@@ -48,7 +48,7 @@ module.exports = {
     },
     compilers: {
         solc: {
-            version: "0.4.25",
+            version: "0.5.0",
         },
     }
     // See <http://truffleframework.com/docs/advanced/configuration>
