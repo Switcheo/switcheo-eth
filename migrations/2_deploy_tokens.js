f6993b1c13697012d7751a3e3f4ca6d36a559adb
John Wong <jwwwwong@gmail.com>
2019-09-03 14:24:28 +0800

    Fix ERC777 token deployment

--- migrations/2_deploy_tokens.js
+++ migrations/2_deploy_tokens.js
@@ -3,8 +3,18 @@ const SWCoin = artifacts.require('SWCoin')
 const DGTX = artifacts.require('DGTX')
 const ZEUS = artifacts.require('ZEUS')
 
-module.exports = function(deployer) {
+require('openzeppelin-test-helpers/configure')({ web3 })
+const { singletons } = require('openzeppelin-test-helpers')
+
+module.exports = function(deployer, network, accounts) {
     deployer.then(async () => {
+        if (network === 'development') {
+            // in a test environment ERC777 tokens require
+            // deploying an ERC1820 registry
+            /* eslint-disable new-cap */
+            await singletons.ERC1820Registry(accounts[0])
+        }
+
         await deployer.deploy(JRCoin)
         await deployer.deploy(SWCoin)
         await deployer.deploy(DGTX)
